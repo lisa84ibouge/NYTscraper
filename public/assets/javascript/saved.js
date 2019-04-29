161d130bfaac75ca06aa1b495cf1f05647230dd7
Lisa Leslie <lisar@ibouge.com>
2019-04-29 00:06:24 -0700

    finished saved.js file

--- public/assets/javascript/saved.js
+++ public/assets/javascript/saved.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     var articleContainer = $(".article-container");
 
@@ -10,106 +10,181 @@ $(document).ready(function(){
 
     initPage();
 
-    function initPage(){
+    function initPage() {
         articleContainer.empty();
-        $.get("/api/headlines?saved=true").then(function(data){
+        $.get("/api/headlines?saved=true").then(function (data) {
 
-        if (data && data.length) {
-            renderArticles(data);
-        } else {
-            renderEmpty();
-        }
+            if (data && data.length) {
+                renderArticles(data);
+            } else {
+                renderEmpty();
+            }
         });
 
     }
 
-function renderArticles(articles){
+    function renderArticles(articles) {
 
-    var articlePanels = [];
+        var articlePanels = [];
 
-    for (var i = 0; i < articles.length; i++){
-        articlePanels.push(createPanel(articles[i]));
+        for (var i = 0; i < articles.length; i++) {
+            articlePanels.push(createPanel(articles[i]));
 
+        }
+
+
+        articleContainer.append(articlePanels);
     }
 
-
-    articleContainer.append(articlePanels);
-}
     function createPanel(article) {
 
-        var panel = 
-        $(["<div class='panel panel-default'>",
-        "<div class='panel-heading'>",
-        "<h3>", 
-        article.headline,
-        "<a class='btn btn-danger delete'>",
-        "Delete From Saved", 
-        "</a>",
-        "<a class='btn btn-info notes'>Article Notes</a>",
-        "</h3>", 
-        "</div>",
-        "<div class='panel-body'>", 
-        article.summary, 
-        "</div>", 
-        "</div>"
-    
-    ]. join(""));
+        var panel =
+            $(["<div class='panel panel-default'>",
+                "<div class='panel-heading'>",
+                "<h3>",
+                article.headline,
+                "<a class='btn btn-danger delete'>",
+                "Delete From Saved",
+                "</a>",
+                "<a class='btn btn-info notes'>Article Notes</a>",
+                "</h3>",
+                "</div>",
+                "<div class='panel-body'>",
+                article.summary,
+                "</div>",
+                "</div>"
 
-    panel.data("_id", article._id);
-    
-    return panel;
-
-
-    }
-
-function renderEmpty() {
-    var emptyAlert = 
-    $(["<div class='alert alert-warning text-center'>",
-        "<h4>Uh Oh. Looks like we don't have any new articles.</h4>",
-        "</div>",
-        "<div class='panel panel-default'>",
-        "<div class='panel-heading text-center'>",
-        "<h3>Would you like to browse avaiable articles?</h3>",
-        "</div>",
-        "<div class='panel-body text-center'>",
-        "<h4><a href='/'> Browse Articles</a></h4>",
-        "</div>",
-        "</div>"
-].join(""));
-articleContainer.append(emptyAlert);
-}
-
-function renderNotesList(data) {
-
-    var notesToRender = [];
-    var currentNote; 
-    if(!data.notes.length){
-        currentNote = [
-            "<li class= 'list-group-item'>",
-            "No notes for this article yet.", 
-            "</li>"
-
-        ].join("");
-        notesToRender.push(currentNote);
-    }
-    else {
-        for(var i = 0; i < data.notes.length; i++) {
-            currentNote = $([
-                "<li class='list-group-item note'>",
-                data.notes[i].noteText, 
-                "<button class='btn btn-danger note-delete'>x</button>",
-                "</li>"
             ].join(""));
 
-            currentNote.children("button").data("_id", data.notes[i]._id);
+        panel.data("_id", article._id);
 
-            notesToRender.push(currentNote);
-        }
+        return panel;
+
+
     }
-    $(".note-container").append(notesToRender);
-}
- function handleArticleDelete() {
-     
- }
 
-})
+    function renderEmpty() {
+        var emptyAlert =
+            $(["<div class='alert alert-warning text-center'>",
+                "<h4>Uh Oh. Looks like we don't have any new articles.</h4>",
+                "</div>",
+                "<div class='panel panel-default'>",
+                "<div class='panel-heading text-center'>",
+                "<h3>Would you like to browse avaiable articles?</h3>",
+                "</div>",
+                "<div class='panel-body text-center'>",
+                "<h4><a href='/'> Browse Articles</a></h4>",
+                "</div>",
+                "</div>"
+            ].join(""));
+        articleContainer.append(emptyAlert);
+    }
+
+    function renderNotesList(data) {
+
+        var notesToRender = [];
+        var currentNote;
+        if (!data.notes.length) {
+            currentNote = [
+                "<li class= 'list-group-item'>",
+                "No notes for this article yet.",
+                "</li>"
+
+            ].join("");
+            notesToRender.push(currentNote);
+        } else {
+            for (var i = 0; i < data.notes.length; i++) {
+                currentNote = $([
+                    "<li class='list-group-item note'>",
+                    data.notes[i].noteText,
+                    "<button class='btn btn-danger note-delete'>x</button>",
+                    "</li>"
+                ].join(""));
+
+                currentNote.children("button").data("_id", data.notes[i]._id);
+
+                notesToRender.push(currentNote);
+            }
+        }
+        $(".note-container").append(notesToRender);
+    }
+
+    function handleArticleDelete() {
+
+        var articleToDelete = $(this).parents(".panel").data();
+
+        $.ajax({
+            method: "DELETE",
+            url: "/api/headlines/" + articleToDelete._id
+        }).then(function (data) {
+
+            if (data.ok) {
+                initPage();
+            }
+        })
+
+    }
+
+
+
+function handleArticleNotes(){
+
+    var currentArticle = $(this).parents(".panel").data();
+   
+    $.get("/api/notes/" + currentArticle._id).then(function(data){
+
+        var modalText = [
+            "<div class='container-fluid text-center'>",
+            "<h4> Notes for Article: ",
+            currentArticle._id,
+            "</h4>",
+            "<hr />",
+            "ul class='list-group note-container'>",
+            "</ul>",
+            "<textarea placeholder='New Note' rows='4' cols'60'></textarea>",
+            "<button class='btn btn-success save'>Save Note</button>"
+
+        ].join("");
+        bootbox.dialog({
+            message: modalText, 
+            closeButton: true
+
+        });
+        var noteData = {
+            _id: currentArticle._id,
+            notes: data || []
+        };
+        $(".btn.save").data("article", noteData);
+        renderNotesList(noteData);
+    });
+}
+
+function handleNoteSave() {
+    var noteData;
+    var newNote = $(".bootbox-body textarea").val().trim();
+
+    if (newNote){
+        noteData = {
+            _id: $(this).data("article")._id,
+            noteText: newNote
+        };
+
+        $.post("/api/notes", noteData).then(function(){
+
+            bootbox.hideAll();
+        });
+    }
+}
+
+function handleNoteDelete() {
+
+    var noteToDelete = $(this).data("_id");
+
+    $.ajax({
+        url: "/api/notes/" + noteToDelete,
+        method: "DELETE"
+    }).then(function(){
+        bootbox.hideAll();
+    });
+}
+});
